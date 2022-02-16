d4e8cc9f7b490977e96a6b3a90607282a548fe50
darizagrl <darizagrl@gmail.com>
2022-02-16 19:12:46 +0200

    create mocked endpoints

--- event-service-rest/src/main/java/event/EventServiceController.java
+++ event-service-rest/src/main/java/event/EventServiceController.java
@@ -1,8 +1,10 @@
 package event;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController("/events")
 public class EventServiceController {
@@ -10,6 +12,31 @@ public class EventServiceController {
     @PostMapping()
     public ResponseEntity<Event> createEvent() {
         return ResponseEntity.ok(new Event());
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable String id) {
+        return ResponseEntity.ok(new Event());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEvent(@PathVariable String id) {
+        return ResponseEntity.ok(new Event());
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEvent(@PathVariable String id) {
+        return "deleted successfully";
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Event>> getAllEvents() {
+        return ResponseEntity.ok(new ArrayList<>());
+    }
+
+    @GetMapping("/titles/{title}")
+    public ResponseEntity<List<Event>> getAllEventsByTitle(@PathVariable String title) {
+        return ResponseEntity.ok(new ArrayList<>());
     }
 
 }
