c466a1de0d16d496fd1b5dfa76bd6d654d171f44
darizagrl <darizagrl@gmail.com>
2022-02-22 11:26:52 +0200

    add Swagger documentation

--- event-service-dto/src/main/java/event/Event.java
+++ event-service-dto/src/main/java/event/Event.java
@@ -1,5 +1,7 @@
 package event;
 
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -14,23 +16,29 @@ import java.time.LocalDateTime;
 @Table(name = "events")
 public class Event {
 
+    @ApiModelProperty(value = "id of the event", name = "id", dataType = "Integer", example = "1")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Integer id;
 
+    @ApiModelProperty(value = "title of the event", name = "title", dataType = "String", example = "Atlas")
     @Column(name = "title")
     private String title;
 
+    @ApiModelProperty(value = "location of the event", name = "place", dataType = "String", example = "Garden")
     @Column(name = "place")
     private String place;
 
+    @ApiModelProperty(value = "speaker at the event", name = "speaker", dataType = "String", example = "Ostrovsky")
     @Column(name = "speaker")
     private String speaker;
 
+    @ApiModelProperty(value = "type of the event", name = "eventType", dataType = "String", example = "Lecture")
     @Column(name = "event_type")
     private String eventType;
 
+    @ApiModelProperty(value = "date and time of the event", name = "dateTime", dataType = "date-time", example = "2022-02-21T19:00:0")
     @Column(name = "date_time")
     private LocalDateTime dateTime;
 
--- event-service-rest/src/main/java/event/EventServiceController.java
+++ event-service-rest/src/main/java/event/EventServiceController.java
@@ -1,5 +1,8 @@
 package event;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api
 @RestController
 @RequestMapping("/events")
 @RequiredArgsConstructor
@@ -14,24 +18,28 @@ public class EventServiceController {
 
     private final EventService service;
 
+    @ApiOperation("Create a new event")
     @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED)
     public ResponseEntity<Event> createEvent(@RequestBody Event event) {
         return ResponseEntity.ok(service.createEvent(event));
     }
 
-    @PutMapping("/{id}")
+    @ApiOperation("Update an existing event")
+    @PutMapping()
     @ResponseStatus(code = HttpStatus.OK)
-    public ResponseEntity<Event> updateEvent(@PathVariable Integer id, @RequestBody Event event) {
+    public ResponseEntity<Event> updateEvent(@RequestBody Event event) {
         return ResponseEntity.ok(service.updateEvent(event));
     }
 
+    @ApiOperation("Get an event by id")
     @GetMapping("/{id}")
     @ResponseStatus(code = HttpStatus.OK)
     public ResponseEntity<Event> getEvent(@PathVariable Integer id) {
         return ResponseEntity.ok(service.getEvent(id));
     }
 
+    @ApiOperation("Delete an event")
     @DeleteMapping("/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public HttpStatus deleteEvent(@PathVariable Integer id) {
@@ -39,16 +47,16 @@ public class EventServiceController {
         return HttpStatus.NO_CONTENT;
     }
 
+    @ApiOperation("Get all events")
     @GetMapping()
     @ResponseStatus(code = HttpStatus.OK)
-    public ResponseEntity<List<Event>> getAllEvents() {
+    public ResponseEntity<List<Event>> getAllEvents(@RequestParam(required = false)
+                                                    @ApiParam(name = "title", type = "String",
+                                                            value = "Title of the event") String title) {
+        if (title != null) {
+            return ResponseEntity.ok(service.getAllEventsByTitle(title));
+        }
         return ResponseEntity.ok(service.getAllEvents());
-    }
-
-    @GetMapping()
-    @ResponseStatus(code = HttpStatus.OK)
-    public ResponseEntity<List<Event>> getAllEventsByTitle(@RequestParam String title) {
-        return ResponseEntity.ok(service.getAllEventsByTitle(title));
     }
 
 }
